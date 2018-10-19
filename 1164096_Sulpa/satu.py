5e89617e1f12c065d3429069cb55028083c69c13
Sulpadianti Bunyamin <37585648+sulpadianti@users.noreply.github.com>
2018-10-19 06:13:21 -0700

    Add files via upload
    kommit hari pertama 2. oke sip

---
+++ 1164096_Sulpa/satu.py
@@ -0,0 +1,14 @@
+import shapefile #menginport modul yang bernama shapefile
+w=shapefile.Writer() #sebagai penndeklarasian variabel
+w.shapeType #menjalankan perintah pendeklarasian variabel diatas
+
+w.field("kolom1","C") #membuat field/kolom1 dengan type data character
+w.field("kolom2","C") #membuat field/kolom2 dengan type data character
+ 
+w.record("ngek","satu") #mengisi kolom1 yang telah dibuat diatas
+w.record("ngok","dua") #mengisi kolom2 yang telah dibuat diatas
+ 
+w.point(1,1) #membuat titik 1,1
+w.point(2,2) #membuat titik 1,2
+ 
+w.save("soal1") #untuk menyimpan soal1
